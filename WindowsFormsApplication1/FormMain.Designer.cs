8f839c6f40ed0ad6d5661de12969c370de60ec59
Akini Digitals <akinidigitals@gmail.com>
2021-11-20 15:43:56 +0800

    Added UserControl

---
+++ WindowsFormsApplication1/FormMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(205, 563);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Dashboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Menu_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "User";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Menu_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.button1);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(205, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Padding = new System.Windows.Forms.Padding(10);
+            this.MainPanel.Size = new System.Drawing.Size(791, 563);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "User";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 563);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.MainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WindowsFormsApplication1/FormMain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.View;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormMain : Form
+    {
+        private DashboardUC dashboarUC;
+        private UserUC userUC;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            switch(btn.Text)
+            {
+                case "Dashboard":
+                    if(!MainPanel.Controls.ContainsKey("DashboardUC"))
+                    {
+                        dashboarUC = new DashboardUC();
+                        dashboarUC.Dock = DockStyle.Fill;
+                        MainPanel.Controls.Add(dashboarUC);
+                    }
+                    MainPanel.Controls["DashboardUC"].BringToFront();
+                    break;
+                case "User":
+                    if (!MainPanel.Controls.ContainsKey("UserUC"))
+                    {
+                        userUC = new UserUC();
+                        userUC.Dock = DockStyle.Fill;
+                        MainPanel.Controls.Add(userUC);
+                    }
+                    userUC.LoadData();
+                    MainPanel.Controls["UserUC"].BringToFront();
+                    break;
+            }
+        }
+    }
+}
